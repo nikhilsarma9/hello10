302eab7a902b9f0f80651b86217bbe35c9fffe25
sainikhil <109243188+nikhilsarma9@users.noreply.github.com>
2022-11-19 04:12:11 -0800

    update

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -1,10 +1,25 @@
 package helloworld;
 
+
+
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("helloworld nikhil");
-
+		
+		int[] a={10,20,30,40,50};
+		for(int elem:a)
+		
+		{
+			System.out.println(" ");
+			System.out.println(elem);
+		}
+		
+		
+		/*for(int i=0;i<a.length();i++)
+		{
+			char[] b=a.toCharArray();
+			System.out.println(b[i]);
+		}*/
 	}
 
 }
